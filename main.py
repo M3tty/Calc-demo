df1ff5c0f1a21fdfdd136b2d1c47825d8fe53d9a
M3tty <m3tticar@gmail.com>
2022-04-17 08:15:06 +0300

    add mul and div func
    *  and / prosessing

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ def sub_nums(a,b):
     return a-b
 
 
+def mul_nums(a,b):
+    return a*b
+
+def div_nums(a,b):
+    return a/b
+
 def main():
     num1 = int(input("Введите 1ое число: "))
     num2 = int(input("Введите 2ое число: "))
     oper = input("Укажите действие: ")
     if oper == '+':
         print(summ(num1, num2))
-    elif oper == '-'
+    elif oper == '-':
         print(sub_nums(num1, num2))
+    elif oper == '*':
+        print(mul_nums(num1, num2))
+    elif oper == '/':
+        print(div_nums(num1, num2))
 
 
 
